df93448ac5847a668e968b39500f34bc6d3cd35d
Thomasdelecluse <delecluse.thom@gmail.com>
2024-06-20 22:22:04 +0200

    feat: update api clean code

--- src/main/java/com/example/apispringjwt/service/impl/ContactService.java
+++ src/main/java/com/example/apispringjwt/service/impl/ContactService.java
@@ -1,16 +1,19 @@
 package com.example.apispringjwt.service.impl;
 
 import com.example.apispringjwt.dto.response.ContactDTO;
+import com.example.apispringjwt.exeption.ResponseEntityException;
 import com.example.apispringjwt.model.Contact;
 import com.example.apispringjwt.model.User;
 import com.example.apispringjwt.repository.IContactRepository;
 import com.example.apispringjwt.repository.IUserRepository;
 import com.example.apispringjwt.service.IContactService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-import java.util.stream.Collectors;
+
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import static com.example.apispringjwt.service.impl.AuthService.getUserNameConnected;
 
@@ -20,18 +23,20 @@ public class ContactService implements IContactService {
     private IContactRepository contactRepository;
     @Autowired
     private IUserRepository userRepository;
+
     public List<ContactDTO> getContactOfUserConnected() {
         Optional<User> userConnected = userRepository.findByEmail(getUserNameConnected());
-
         if (userConnected.isPresent()) {
             List<Contact> contacts = contactRepository.findByUserId_Id(userConnected.get().getId());
             if (!contacts.isEmpty()) {
                 return contacts.stream()
                         .map(contact -> new ContactDTO(contact.getContactEmail(), contact.getTelephone()))
                         .collect(Collectors.toList());
+            } else {
+                throw new ResponseEntityException(HttpStatus.NO_CONTENT, "No contact found");
             }
         }
-        return null;
+        throw new ResponseEntityException(HttpStatus.NO_CONTENT, "No User Found");
     }
 
 }
